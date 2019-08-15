08dc0fb97385e44b27907d4cbfd7bf56f874df0c
sergeresko <syeresko@e1r4p8.unit.ua>
2019-08-15 17:41:53 +0300

    Minify

--- sastantua.c
+++ sastantua.c
@@ -6,14 +6,14 @@
 /*   By: syeresko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/12 19:31:32 by syeresko          #+#    #+#             */
-/*   Updated: 2019/08/15 15:36:34 by syeresko         ###   ########.fr       */
+/*   Updated: 2019/08/15 16:16:28 by syeresko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void		ft_putchar(char c);
 
 int		g_c;
-int		g_m;
+int		g_mid;
 int		g_left;
 int		g_right;
 
@@ -22,22 +22,20 @@ static void	print_line(int n, int s, int t)
 	int		i;
 
 	i = 0;
-	while (i < g_left)
-	{
+	while (i++ < g_left)
 		ft_putchar(' ');
-		++i;
-	}
 	ft_putchar('/');
-	++i;
-	while (i < g_right)
+	--i;
+	while (++i < g_right)
 	{
-		if (s < n || t <= 3 - n % 2 || i < g_m - g_c || i > g_m + g_c)
+		if (s < n || t <= 3 - n % 2 || i < g_mid - g_c || i > g_mid + g_c)
 			ft_putchar('*');
-		else if (n > 4 && t == n + 2 - g_c && i == g_m - 1 + g_c)
-			ft_putchar('$');
 		else
-			ft_putchar('|');
-		++i;
+			ft_putchar("|$"[n > 4 && t == n + 2 - g_c && i == g_mid - 1 + g_c]);
+		//else if (n > 4 && t == n + 2 - g_c && i == g_mid - 1 + g_c)
+		//	ft_putchar('$');
+		//else
+		//	ft_putchar('|');
 	}
 	ft_putchar('\\');
 	ft_putchar('\n');
@@ -62,9 +60,9 @@ void		sastantua(int n)
 	int		s;
 
 	g_c = (n - 1) / 2;
-	g_m = n * (n + 5) / 2 + g_c * (g_c + 3) + (n % 2 ? 0 : g_c + 2);
-	g_left = g_m - 1;
-	g_right = g_m + 1;
+	g_mid = n * (n + 5) / 2 + g_c * (g_c + 3) + (n % 2 ? 0 : g_c + 2);
+	g_left = g_mid - 1;
+	g_right = g_mid + 1;
 	s = 1;
 	while (s <= n)
 	{
